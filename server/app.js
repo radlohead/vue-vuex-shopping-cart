79e946acd787466980320aa42b08adef40a64845
= <radlohead@naver.com>
2019-06-29 14:53:09 +0900

    api/coffee json is structure modify

--- server/app.js
+++ server/app.js
@@ -6,7 +6,22 @@ const connection = mysql.createConnection(userInfo)
 
 app.get('/api/coffees', (req, res, next) => {
   connection.query('SELECT * FROM coffees', (err, results, fields) => {
-    res.send(results)
+    const tags = results.map(v => {
+      return {
+        key: v['tags.key'],
+        name: v['tags.name']
+      }
+    })
+    const deletePropertys = results.map(v => {
+      delete v['tags.key']
+      delete v['tags.name']
+      return v
+    })
+    const result = deletePropertys.map((v, i) => {
+      v.tags = tags[i]
+      return v
+    })
+    res.send(result)
   })
 })
 app.listen(4000, () => {
