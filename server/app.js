106b767ebee8d9a7d28266e85f449977ead7f36e
= <radlohead@naver.com>
2019-06-30 11:35:34 +0900

    cors issue modify

--- server/app.js
+++ server/app.js
@@ -4,6 +4,17 @@ const app = express()
 const userInfo = require('./userInfo')
 const connection = mysql.createConnection(userInfo)
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
+  res.header('Access-Control-Request-Method', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, Secrete_Token'
+  )
+  next()
+})
+
 app.get('/api/tags', (req, res, next) => {
   connection.query('SELECT * FROM tags', (err, result, fields) => {
     res.send(result)
